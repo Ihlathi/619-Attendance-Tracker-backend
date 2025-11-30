0f5a15535a79a2f80d22cfba92f4160197977650
Liam Smith <ihlathimc@gmail.com>
2025-11-29 23:03:29 -0500

    fixed cors

--- Code.js
+++ Code.js
@@ -4,9 +4,29 @@
  */
 
 function doGet(e) {
-    return handleRequest(e);
+  // service ok
+  var output = ContentService.createTextOutput(
+    JSON.stringify({ status: 'ok', message: 'Backend is running.' })
+  );
+  return withCors(output);
 }
 
 function doPost(e) {
-    return handleRequest(e);
+    return withCors(handleRequest(e));
+}
+
+function doOptions(e) {
+  // The body can be empty; we only need to send the CORS headers.
+  var output = ContentService.createTextOutput('');
+  return withCors(output);
+}
+
+// shut CORS up
+function withCors(output) {
+  return output
+    .setMimeType(ContentService.MimeType.JSON)
+    .setHeader('Access-Control-Allow-Origin', '*')
+    .setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    .setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    .setHeader('Access-Control-Max-Age', '86400'); // cache pre‑flight for 1 day
 }
